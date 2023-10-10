c2a0503b4b5f5e026c60f0043caf232ccc2e87d8
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-10 12:38:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Student student1 = new Student(70, "John", 35);
+            Student student2 = new Student(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.Read();
+
+        
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_methods
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aName;
+            this.age = aAge;
+        }
+
+        // you now have a one stop shop to test obesity of your students
+
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return false;
+            }
+            else return true;
+        }
+    }
+}
+
+
+        
+    
+
